c9e57a2793a1273fce87ae8f09df6f35e49ed02e
mimiaczekpluszaczek <mimi020001@gmail.com>
2024-08-25 23:11:43 +0200

    Initial Commit

---
+++ Bidon/BidonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidonApp.swift
+//  Bidon
+//
+//  Created by Michal on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BidonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
